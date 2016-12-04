6439075235b13f71e8637ab5ccb694b63972c774
LEBEAU Christophe <c.lebeau23@gmail.com>
2016-12-04 23:06:28 +0100

    more flexible "or" operator for the transformers

--- src/main/scala/com/rokuan/autoroute/matchers/Transformer.scala
+++ src/main/scala/com/rokuan/autoroute/matchers/Transformer.scala
@@ -8,7 +8,7 @@ import com.rokuan.autoroute.rules._
   * Created by Christophe on 23/11/2016.
   */
 trait Transformer[T <: Any, K, +R] extends Rule[R, K]{
-  def |[R1 >: R, S <: Any](transformer: BasicTransformer[S, K, R1]): Transformer[Any, K, R1]
+  def |[R1 >: R, S <: Any](transformer: Transformer[S, K, R1]): Transformer[Any, K, R1]
   def apply[R1 >: R, S](matcher: R1 => S) = new BasicTransformer[R1, K, S](this, matcher)
 }
 
@@ -18,7 +18,7 @@ class BasicTransformer[T, K, +R](val rule: Rule[T, K], val transform: T => R) ex
       case (result, left) => Some(transform(result), left)
     }.getOrElse(None)
 
-  override def |[R1 >: R, S <: Any](transformer: BasicTransformer[S, K, R1]): Transformer[Any, K, R1] =
+  override def |[R1 >: R, S <: Any](transformer: Transformer[S, K, R1]): Transformer[Any, K, R1] =
     new MultipleTransformer[K, R1](List(this, transformer))
 
   override def apply[R1 >: R, S](matcher: R1 => S) = new BasicTransformer[R1, K, S](this, matcher)
@@ -38,7 +38,7 @@ class MultipleTransformer[K, +R](val transformers: List[Transformer[_ <: Any, K,
     internalProduct(transformers, p)
   }
 
-  override def |[R1 >: R, S <: Any](transformer: BasicTransformer[S, K, R1]): Transformer[Any, K, R1] =
+  override def |[R1 >: R, S <: Any](transformer: Transformer[S, K, R1]): Transformer[Any, K, R1] =
     new MultipleTransformer[K, R1](transformers :+ transformer)
 
   override def apply[R1 >: R, S](matcher: R1 => S) = new BasicTransformer[R1, K, S](this, matcher)
